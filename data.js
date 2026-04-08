2f57e8ef8d1eb561b3f1f6acf85a38f73a7ba59e
iheardthatsound123-cyber <iheardthatsound123@gmail.com>
2026-04-08 08:09:37 +0300

    Обновление 08.04.2026, 08:09:37

--- data.js
+++ data.js
@@ -1,5 +1,84 @@
 window.SHIPMENT_DATA = {
-  "ready": [],
+  "ready": [
+    {
+      "customer": "СТРОИТЕЛЬНАЯ КОМПАНИЯ ВПМ (221)",
+      "items": [
+        {
+          "serial": "31091167",
+          "description": "(3000х3000 ННН__К RAL7035)"
+        },
+        {
+          "serial": "31091168",
+          "description": "(3000х3000 ННН__К RAL7035)"
+        }
+      ]
+    },
+    {
+      "customer": "ДЕЛЬТА (208)",
+      "items": [
+        {
+          "serial": "31091169",
+          "description": "(2000х3000 ННН__К RAL9016)"
+        },
+        {
+          "serial": "31091170",
+          "description": "(2000х3000 ННН__К RAL9016)"
+        }
+      ]
+    },
+    {
+      "customer": "МЕГА-МОНТАЖ (225)",
+      "items": [
+        {
+          "serial": "31081171",
+          "description": "(1100х1000 ННН__К RAL9016)"
+        }
+      ]
+    },
+    {
+      "customer": "АВ ГРУПП ООО (228)",
+      "items": [
+        {
+          "serial": "31231172",
+          "description": "(4000х5000 ННН__К RAL7035)"
+        },
+        {
+          "serial": "31231173",
+          "description": "(4000х5000 ННН__К RAL7035)"
+        },
+        {
+          "serial": "31171174",
+          "description": "(3000х5000 ННН__К RAL7035)"
+        },
+        {
+          "serial": "31171175",
+          "description": "(3000х5000 ННН__К RAL7035)"
+        },
+        {
+          "serial": "31171176",
+          "description": "(3120х5000 ННН__К RAL7035)"
+        }
+      ]
+    },
+    {
+      "customer": "СПРИНТ (226)",
+      "items": [
+        {
+          "serial": "31091177",
+          "description": "(760х2130 ННН__К RAL7035)"
+        }
+      ]
+    },
+    {
+      "customer": "АЛЬЯНС-СВ (220)",
+      "items": [
+        {
+          "serial": "31081178",
+          "description": "(1100х1000 ННН__К RAL9016)"
+        }
+      ]
+    }
+  ],
   "shipped": [],
-  "updated": "2026-04-08T05:05:55.722Z"
+  "updated": "2026-04-08T05:09:36.410Z"
 };
